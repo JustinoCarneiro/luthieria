36b67a133946e36fb85d22307f475951230a25b9
Justino Carneiro <justinocarneiro161@gmail.com>
2024-09-06 16:36:23 -0300

    Changes to be committed:     modified:   bin/Luthieria.class     new file:   bin/TestePainel/MinhaJanela.class     modified:   src/Luthieria.java     new file:   src/TestePainel/MinhaJanela.java

--- src/Luthieria.java
+++ src/Luthieria.java
@@ -1,5 +1,8 @@
+import TestePainel.MinhaJanela;
+
 public class Luthieria {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        MinhaJanela frame = new MinhaJanela("Titulo da Aplicacao");
+        frame.setVisible(true);
     }
 }
---
+++ src/TestePainel/MinhaJanela.java
@@ -0,0 +1,17 @@
+package TestePainel;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MinhaJanela extends JFrame{
+    public MinhaJanela(String titulo){
+        super(titulo);
+        JPanel pane = new JPanel();
+        JButton botao = new JButton ("Texto botao");
+        pane.add(botao);
+        setContentPane(pane);
+        pack();
+        setVisible(true);
+    }
+}
