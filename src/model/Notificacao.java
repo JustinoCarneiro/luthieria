be58f731285a377ecad3b60780a56d3d119a47a7
Justino Carneiro <justinocarneiro161@gmail.com>
2024-09-13 17:04:50 -0300

    Changes to be committed: 	modified:   bin/model/Notificacao.class 	modified:   bin/model/OrdemServico.class 	modified:   bin/repositorio/IRepositorioCliente.class 	modified:   bin/repositorio/IRepositorioInstrumento.class 	modified:   bin/repositorio/IRepositorioOrdemServico.class 	modified:   bin/repositorio/IRespositorioNotificacao.class 	modified:   src/model/Notificacao.java 	modified:   src/model/OrdemServico.java

--- src/model/Notificacao.java
+++ src/model/Notificacao.java
@@ -4,24 +4,12 @@ import java.util.UUID;
 
 public class Notificacao {
     private UUID id;
-    private String nomeInstrumento;
-    private String nomePropietario;
-    private String statusInstrumento;
-    private String observacaoStatus;
-    private String previsaoEntrega;
-    private Integer ordemServico;
-    private String necessidadeMateriais;
+    private OrdemServico ordemServico;
     
-    public Notificacao(UUID id, String nomeInstrumento, String nomePropietario, String statusInstrumento,
-            String observacaoStatus, String previsaoEntrega, Integer ordemServico, String necessidadeMateriais) {
+    
+    public Notificacao(UUID id, OrdemServico ordemServico) {
         this.id = id;
-        this.nomeInstrumento = nomeInstrumento;
-        this.nomePropietario = nomePropietario;
-        this.statusInstrumento = statusInstrumento;
-        this.observacaoStatus = observacaoStatus;
-        this.previsaoEntrega = previsaoEntrega;
         this.ordemServico = ordemServico;
-        this.necessidadeMateriais = necessidadeMateriais;
     }
     
     public UUID getId() {
@@ -30,47 +18,11 @@ public class Notificacao {
     public void setId(UUID id) {
         this.id = id;
     }
-    public String getNomeInstrumento() {
-        return nomeInstrumento;
-    }
-    public void setNomeInstrumento(String nomeInstrumento) {
-        this.nomeInstrumento = nomeInstrumento;
-    }
-    public String getNomePropietario() {
-        return nomePropietario;
-    }
-    public void setNomePropietario(String nomePropietario) {
-        this.nomePropietario = nomePropietario;
-    }
-    public String getStatusInstrumento() {
-        return statusInstrumento;
-    }
-    public void setStatusInstrumento(String statusInstrumento) {
-        this.statusInstrumento = statusInstrumento;
-    }
-    public String getObservacaoStatus() {
-        return observacaoStatus;
-    }
-    public void setObservacaoStatus(String observacaoStatus) {
-        this.observacaoStatus = observacaoStatus;
-    }
-    public String getPrevisaoEntrega() {
-        return previsaoEntrega;
-    }
-    public void setPrevisaoEntrega(String previsaoEntrega) {
-        this.previsaoEntrega = previsaoEntrega;
-    }
-    public Integer getOrdemServico() {
+   
+    public OrdemServico getOrdemServico() {
         return ordemServico;
     }
-    public void setOrdemServico(Integer ordemServico) {
+    public void setOrdemServico(OrdemServico ordemServico) {
         this.ordemServico = ordemServico;
     }
-    public String getNecessidadeMateriais() {
-        return necessidadeMateriais;
-    }
-    public void setNecessidadeMateriais(String necessidadeMateriais) {
-        this.necessidadeMateriais = necessidadeMateriais;
-    }
-
 }
--- src/model/OrdemServico.java
+++ src/model/OrdemServico.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.time.LocalDate;
 import java.util.UUID;
 import java.util.Vector;
 
@@ -14,9 +15,12 @@ public class OrdemServico {
     private Cliente cliente;
     private double valorServico;
     private Vector<String> pecas;
+    private String statusInstrumento;
+    private String observacaoStatus;
+    private LocalDate previsaoEntrega;
 
     public OrdemServico(UUID id, Integer codigo, String tipoServico, Instrumento instrumento, Cliente cliente,
-            double valorServico, Vector<String> pecas) {
+            double valorServico, Vector<String> pecas, String statusInstrumento, String observacaoStatus, LocalDate previsaoEntrega) {
         this.id = id;
         this.codigo = codigo;
         this.tipoServico = tipoServico;
@@ -24,6 +28,9 @@ public class OrdemServico {
         this.cliente = cliente;
         this.valorServico = valorServico;
         this.pecas = pecas;
+        this.statusInstrumento = statusInstrumento;
+        this.observacaoStatus = observacaoStatus;
+        this.previsaoEntrega = previsaoEntrega;
     }
 
     public UUID getId() {
@@ -69,5 +76,27 @@ public class OrdemServico {
         this.pecas = pecas;
     }
 
+    public String getStatusInstrumento() {
+        return statusInstrumento;
+    }
 
+    public void setStatusInstrumento(String statusInstrumento) {
+        this.statusInstrumento = statusInstrumento;
+    }
+
+    public String getObservacaoStatus() {
+        return observacaoStatus;
+    }
+
+    public void setObservacaoStatus(String observacaoStatus) {
+        this.observacaoStatus = observacaoStatus;
+    }
+
+    public LocalDate getPrevisaoEntrega() {
+        return previsaoEntrega;
+    }
+
+    public void setPrevisaoEntrega(LocalDate previsaoEntrega) {
+        this.previsaoEntrega = previsaoEntrega;
+    }
 }
