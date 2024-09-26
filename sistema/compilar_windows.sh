86736a421bd13b42b5a2155a91f3a943909faf1e
JustinoCarneiro <justinocarneiro161@gmail.com>
2024-09-26 11:06:54 -0300

    Mudanças a serem submetidas: 	modified:   sistema/compilar.sh 	modified:   sistema/compilar_windows.sh

--- sistema/compilar_windows.sh
+++ sistema/compilar_windows.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-# Diretório de destino para os arquivos compilados
 DESTINO="bin"
 
-# Caminho para o driver JDBC do PostgreSQL (ajuste o caminho se necessário)
 DRIVER_PATH="C:/Program Files/PostgreSQL/16/postgresql-42.6.2.jar"
 
 rm -rf $DESTINO
 mkdir -p $DESTINO
 
-# Compilar todos os arquivos .java dentro de src e subdiretórios
 javac -d $DESTINO -cp "src;$DRIVER_PATH" src/**/*.java
 
-# Copiar arquivos de recursos (imagens, etc.) para o diretório de destino
 cp -r src/gui/arquivos $DESTINO/gui/
 
-# Verificar se a compilação foi bem-sucedida antes de rodar o programa
 if [ $? -eq 0 ]; then
   echo "Compilação concluída com sucesso!"
   
-  # Executar o programa principal (classe Luthieria) com o driver JDBC no classpath
   java -cp "$DESTINO;$DRIVER_PATH" inicio.Luthieria
 else
   echo "Erro na compilação."
